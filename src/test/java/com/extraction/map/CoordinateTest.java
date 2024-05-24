a1c831e073a08ba6c3c6d6e074f64e20b33c19fc
pose2003 <posenato2003@gmail.com>
2024-05-24 16:08:50 +0200

    Added CoordinateTest

--- src/test/java/com/extraction/map/CoordinateTest.java
+++ src/test/java/com/extraction/map/CoordinateTest.java
@@ -1,7 +1,36 @@
 package com.extraction.map;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CoordinateTest {
 
+    @ParameterizedTest
+    @ValueSource(strings = {"0,0", "0,10", "10,0", "100,100"})
+    void testValidConstructor(String value) {
+        Coordinate coordinate = new Coordinate(value);
+        int row = coordinate.getRow();
+        int column = coordinate.getColumn();
+        String sym = row + "," + column;
+        assertEquals(value, sym);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"n,0", "0,s", "k,k"})
+    void testInvalidConstructor(String value) {
+        assertThrows(NumberFormatException.class, () -> new Coordinate(value));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"35,22", "0,7", "98,1"})
+    void testEquals(String value) {
+        Coordinate coordinate1 = new Coordinate(value);
+        Coordinate coordinate2 = new Coordinate("0,0");
+        assertFalse(coordinate1.equals(coordinate2));
+        coordinate2.setRow(coordinate1.getRow());
+        coordinate2.setColumn(coordinate1.getColumn());
+        assertTrue(coordinate1.equals(coordinate2));
+    }
 }
