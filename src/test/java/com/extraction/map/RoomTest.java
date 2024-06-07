de6b61330d66aeaa6bfc83f51cac5aaeb09cc6b8
pose2003 <posenato2003@gmail.com>
2024-06-07 22:57:21 +0200

    Updated PlayerTest and RoomTest.

--- src/test/java/com/extraction/map/RoomTest.java
+++ src/test/java/com/extraction/map/RoomTest.java
@@ -18,53 +18,12 @@ class RoomTest {
     }
 
     @Test
-    void testAdd_Remove_FindKey() {
+    void testAdd_Remove_FindItem() {
         assertNull(room.findItem("Key"));
         room.addItem(new Key());
         assertNotNull(room.findItem("Key"));
         room.removeItem("Key");
-        //not throwable item
-        assertNotNull(room.findItem("Key"));
-    }
-
-    @Test
-    void testAdd_Remove_FindMedKit() {
-        assertNull(room.findItem("MedKit"));
-        room.addItem(new MedKit());
-        assertNotNull(room.findItem("MedKit"));
-        room.removeItem("MedKit");
-        //throwable item
-        assertNull(room.findItem("MedKit"));
-    }
-
-    @Test
-    void testAdd_Remove_FindShield() {
-        assertNull(room.findItem("Shield"));
-        room.addItem(new Shield());
-        assertNotNull(room.findItem("Shield"));
-        room.removeItem("Shield");
-        //throwable item
-        assertNull(room.findItem("Shield"));
-    }
-
-    @Test
-    void testAdd_Remove_FindTNT() {
-        assertNull(room.findItem("TNT"));
-        room.addItem(new TNT());
-        assertNotNull(room.findItem("TNT"));
-        room.removeItem("TNT");
-        //throwable item
-        assertNull(room.findItem("TNT"));
-    }
-
-    @Test
-    void testAdd_Remove_FindTorch() {
-        assertNull(room.findItem("Torch"));
-        room.addItem(new Torch());
-        assertNotNull(room.findItem("Torch"));
-        room.removeItem("Torch");
-        //not throwable item
-        assertNotNull(room.findItem("Torch"));
+        assertNull(room.findItem("Key"));
     }
 
     @ParameterizedTest
